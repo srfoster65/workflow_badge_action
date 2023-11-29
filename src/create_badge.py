901801f115dbcd23b539c02229c25bb880a4f064
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-11-29 23:00:38 +0000

    use badgen to create badge

---
+++ src/create_badge.py
@@ -0,0 +1,92 @@
+"""
+Script to construct and call a Badgen URL and fetch the resultant badge svg data
+"""
+
+from argparse import ArgumentParser
+import requests
+import logging
+
+
+OUTCOME_COLOUR_MAP = {
+    "success": "green",
+    "failure": "red",
+    "cancelled": "grey",
+    "skipped": "grey",
+}
+
+
+BADGEN_URL = "http://badgen.net/badge"
+logger = logging.getLogger(__name__)
+
+def process_command_line_arguments():
+    parser = ArgumentParser(description="Create a workflow badge")
+    parser.add_argument(
+        "-l", "--label",
+        required=True,
+        dest="label",
+        help="The left hand badge text",
+    )
+    parser.add_argument(
+        "-s", "--status",
+        required=True,
+        dest="status",
+        help="The right hand badge text",
+    )
+    parser.add_argument(
+        "-c", "--color", "--colour",
+        dest="colour",
+        help="The background status colour",
+    )
+    parser.add_argument(
+        "-i", "--icon",
+        dest="icon",
+        default="github",
+        help="The icon to use",
+    )
+    parser.add_argument(
+        "-p", "--path",
+        dest="path",
+        required=True,
+        help="The path to save the badge to",
+    )
+    
+    return parser.parse_args()
+
+
+def get_badge_colour(args):
+    """Return background colour for status part of badge."""
+    if args.colour:
+        logger.info("Colour param provided")
+        colour = args.colour
+    elif args.status in OUTCOME_COLOUR_MAP:
+        logger.info("Deriving colour from Status value")
+        colour = OUTCOME_COLOUR_MAP[args.status]
+    else:
+        logger.info("Using default colour")
+        colour = "blue"
+    logger.info("Using colour: %s", colour)
+    return colour
+
+def get_badgen_badge(args):
+    """Generate badge from badgen.net"""
+    colour = get_badge_colour(args)
+    url = f"{BADGEN_URL}/{args.label}/{args.status}/{colour}"
+    params = {'icon': args.icon}
+    response = requests.get(url, params=params)
+    logger.info("URL: %s", response.url)
+    return response.text
+
+
+def write_badge(path, badge_svg):
+    with open(path, "w", encoding="utf-8") as fp:
+        fp.write(badge_svg)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    args = process_command_line_arguments()
+    badge_svg = get_badgen_badge(args)
+    write_badge(args.path, badge_svg)
+
+if __name__ == "__main__":
+    main()
